1700707e93d05d5538cdd705d038a62e184fe487
The Medeiros <91074427+themedeiros@users.noreply.github.com>
2024-08-13 20:58:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var sim = document.getElementById('sim');
+sim.addEventListener("click", function(){
+    alert("Parabéns, você selecionou a opção correta!");
+})
+var nao = document.getElementById('nao');
+nao.addEventListener("click", function(){
+    alert("Selecionou a opção errada!!!!!!!!!!!!!!!!!!!!");
+})
